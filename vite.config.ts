5269877332f6db3c18ae9474ca507c0ce1af60cc
Mathis <eyzex@hotmail.fr>
2022-11-25 12:54:59 +0100

    feat registered app as pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    VitePWA({
+      injectRegister: 'auto',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'Doc App',
+        short_name: 'DocApp',
+        description: 'une appli PWA',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'vite.svg',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'vite.svg',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
 });
